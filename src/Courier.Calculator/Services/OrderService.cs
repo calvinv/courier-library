dcb15b96b712d195b5ed782a6516ebe30bcfd86d
Calvin Verster <calvin.verster@liberis.co.uk>
2020-07-08 21:05:21 +0100

    getting parcel tests working

--- src/Courier.Calculator/Services/OrderService.cs
+++ src/Courier.Calculator/Services/OrderService.cs
@@ -16,15 +16,36 @@ namespace Courier.Calculator.Services
 
         public DeliveryOrder AddParcelToOrder(DeliveryOrder deliveryOrder, int length, int breadth, int height)
         {
+            var newParcel = new Parcel()
+            {
+                Length = length,
+                Breadth = breadth,
+                Height = height,
+            };
+
             if (length < _smallDimension && breadth < _smallDimension && height < _smallDimension)
             {
-                deliveryOrder.Parcels.Add(new Parcel()
-                {
-                    Length = length,
-                    Breadth = breadth,
-                    Height = height,
-                    ParcelType = ParcelType.Small, 
-                    Cost = 3 });
+                newParcel.Cost = 3;
+                newParcel.ParcelType = ParcelType.Small;
+                deliveryOrder.Parcels.Add(newParcel);
+            }
+            else if (length < _mediumDimension && breadth < _mediumDimension && height < _mediumDimension)
+            {
+                newParcel.Cost = 8;
+                newParcel.ParcelType = ParcelType.Medium;
+                deliveryOrder.Parcels.Add(newParcel);
+            }
+            else if (length < _largeDimension && breadth < _largeDimension && height < _largeDimension)
+            {
+                newParcel.Cost = 15;
+                newParcel.ParcelType = ParcelType.Large;
+                deliveryOrder.Parcels.Add(newParcel);
+            }
+            else
+            {
+                newParcel.Cost = 25;
+                newParcel.ParcelType = ParcelType.ExtraLarge;
+                deliveryOrder.Parcels.Add(newParcel);
             }
 
             deliveryOrder.TotalCost = deliveryOrder.Parcels.Sum(x => x.Cost);
